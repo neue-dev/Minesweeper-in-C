e2cfc401099bf951555a006ff6cf12b1d30f945e
Mo David <modavid.1964@gmail.com>
2024-02-16 18:25:40 +0800

    Vector class draft implementation.

--- src/utils/utils.vector.h
+++ src/utils/utils.vector.h
@@ -5,9 +5,70 @@
 #ifndef UTILS_VECTOR
 #define UTILS_VECTOR
 
+#include <stdlib.h>
+
+/**
+ * The vector class helps us store info like screen coordinates or grid dimensions.
+ * Note that the components of the vector are integers as we do not need float values
+ *    for this use case.
+ * 
+ * @class
+*/
 typedef struct Vec2D {
   int x;
   int y;
 } Vec2D;
+
+/**
+ * Allocates memory for an instance of the vector class.
+ * 
+ * @return  { Vec2D * }   An instance of the Vec2D class.
+*/
+Vec2D *Vec2D_new() {
+  Vec2D *pVec2D = calloc(1, sizeof(*pVec2D));
+
+  if(pVec2D == NULL)
+    return NULL;
+  return pVec2D;
+}
+
+/**
+ * Initializes a new instance of the Vec2D class.
+ * 
+ * @param   { Vec2D * }   this  A pointer to the instance to be initialized.
+ * @param   { int }       x     The x-coordinate stored by the instance.
+ * @param   { int }       y     The y-coordinate stored by the instance.
+ * @return  { Vec2D * }         A pointer to the initialized class.
+*/
+Vec2D *Vec2D_init(Vec2D *this, int x, int y) {
+  this->x = x;
+  this->y = y;
+
+  return this;
+}
+
+/**
+ * Creates an initialized instance of the Vec2D class based on the passed values.
+ * 
+ * @param   { int }       x   The x-coordinate stored by the instance.
+ * @param   { int }       y   The y-coordinate stored by the instance.
+ * @return  { Vec2D * }       A pointer to the initialized class.
+*/
+Vec2D *Vec2D_create(int x, int y) {
+  this->x = x;
+  this->y = y;
+
+  return this;
+}
+
+/**
+ * Destroys an instance of the Vec2D class.
+ * Helps with garbage collection.
+ * 
+ * @param   { Vec2D * }   this  The instance to be freed from memory.
+*/
+void Vec2D_kill(Vec2d *this) {
+  free(this);
+}
 
 #endif
