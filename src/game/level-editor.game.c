469901ab92e63e3b0ee1db551116aaf01da69436
mariellajeanne <mariellajeannedellosa@gmail.com>
2024-03-24 16:46:41 +0800

    Added basic code for the level editor.

---
+++ src/game/level-editor.game.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void LevelEditor_createLevel() {
+
+    // TODO: input file name
+    // TODO: check if file name is valid
+    // TODO: check if file name exists
+
+    // TODO: enter number of rows and columns
+
+    // TODO: place mines
+    // TODO: delete mines
+
+    // TODO: check if mine placement is valid
+    // TODO: finish and save
+
+}
+
+int LevelEditor_isValidName() {
+
+}
+
+int LevelEditor_isValidField() {
+
+}
+
+void LevelEditor_saveFile() {
+
+}
+
+
