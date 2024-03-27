9204f7ea6488f1807c7b04f5ba34c6ab67f911ac
mariellajeanne <mariellajeannedellosa@gmail.com>
2024-03-27 23:15:55 +0800

    Considerably revised gameplay.game.c.

--- src/game/gameplay.game.c
+++ src/game/gameplay.game.c
@@ -1,7 +1,7 @@
 /**
  * @ Author: MMMM
  * @ Create Time: 2024-03-21 7:16:46
- * @ Modified time: 2024-03-26 21:57:27
+ * @ Modified time: 2024-03-27 23:14:40
  * @ Description:
  * 
  * Executes tasks involved in-game.
@@ -14,195 +14,209 @@
 
 #include "../utils/utils.file.h"
 #include "../utils/utils.grid.h"
+#include "../utils/utils.string.h"
 
 #include <stdio.h>
 #include <string.h>
 
+#define LEVELS_FILE_PATH "../data/levels.data.txt"
+#define LEVELS_FOLDER_PATH ".../build/levels/"
+
+#define LEVEL_NAME_MAX_LENGTH (1 << 4)
+#define LEVEL_PATH_MAX_SIZE strlen(LEVELS_FOLDER_PATH) + strlen(LEVEL_NAME_MAX_LENGTH) + sizeof(char)*5
+
+#define GAME_EASY_ROWS     8
+#define GAME_EASY_COLUMNS  8
+#define GAME_EASY_MINES    10
+
+#define GAME_DIFFICULT_ROWS    10
+#define GAME_DIFFICULT_COLUMNS 15 
+#define GAME_DIFFICULT_MINES   35
+
 typedef enum GameType GameType;
 typedef enum GameDifficulty GameDifficulty;
-
-typedef struct Game Game;
+typedef enum GameEnding GameEnding;
 
 // Game types chosen by the user
 enum GameType {
-    GAME_TYPE_CLASSIC,      // Classic game type
-    GAME_TYPE_CUSTOM        // Custom game type
+    GAMEPLAY_TYPE_CLASSIC,      // Classic game type
+    GAMEPLAY_TYPE_CUSTOM        // Custom game type
 };
 
 enum GameDifficulty {
-    GAME_DIFFICULTY_EASY,           // Classic game: Easy
-    GAME_DIFFICULTY_DIFFICULT,      // Classic game: Difficult
-    GAME_DIFFICULTY_NONE            // Custom game: No difficulty
+    GAMEPLAY_DIFFICULTY_EASY,           // Classic game: Easy
+    GAMEPLAY_DIFFICULTY_DIFFICULT       // Classic game: Difficult
 };
 
-struct Game {
-    GameType eGameType;
-    GameDifficulty eGameDifficulty;
-
-    Field field;
+enum GameEnding {
+    GAMEPLAY_ENDS_BY_QUITTING,       // The game ends by the player quitting manually
+    GAMEPLAY_ENDS_BY_LOSING,         // The game ends by the player losing
+    GAMEPLAY_ENDS_BY_WINNING         // The game ends by the player winning
 };
 
 /**
- * Sets the game type based on the player's input.
- *
- * @param   { Game * }      this        The game to modify. 
- * @param	{ GameType }    eGameType   The game type.
+ * Initiates the gameplay.
+ * 
 */
-void Gameplay_setType(Game *this, GameType eGameType) {
-    this->eGameType = eGameType;
+void Gameplay_start() {
+    // TODO: Code this function considering the inputs and GUI.
 }
 
 /**
- * Sets the game's difficulty based on the player's input.
+ * Selects the type of game (classic or custom) to be played.
  * 
- * @param   { Game * }              this                The game to modify. 
- * @param	{ GameDifficulty }      eGameDifficulty     The game difficulty.
+ * @param   { GameType }    eType    The type of game to be played.
+ * @param   { Field * }     pField   The field to be modified.
 */
-void Gameplay_setDifficulty(Game *this, GameDifficulty eGameDifficulty) {
-    this->eGameDifficulty = eGameDifficulty;
-}
+void Gameplay_selectType(GameType eType, Field *pField) {
 
-/**
- * Sets up the game's field.
- * 
- * @param   { Game * }              this                 A pointer to the game's field.
- * @param   { Game Type }           eGameType            Game type chosen by the player.
- * @param   { Game Difficulty }     eGameDifficulty      Game difficulty chosen by the player.
-*/
-void Gameplay_setUpField(Game *this, GameType eGameType, GameDifficulty eGameDifficulty) {
+    // For classic games
+    if(eType == GAMEPLAY_TYPE_CLASSIC) {
 
-    // Retrieve the field from the game object
-    Field *pField = &this->field;
+        GameDifficulty eDifficulty;
+        // TODO: input game difficulty
 
-    // Game type: Classic
-    if(eGameType == GAME_TYPE_CLASSIC) {
+        Gameplay_initClassic(eDifficulty, pField);
 
-        // Game difficulty: Easy
-        if(eGameDifficulty == GAME_DIFFICULTY_EASY) {
-            Field_init(pField, 8, 10);      // Sets the field's size to 8 rows x 10 cols
-            Field_populate(pField, 10);     // Populates the field with 10 mines
-        
-        // Game difficulty: Difficult
-        } else {
-            Field_init(pField, 10, 15);     // Sets the field's size to 10 rows x15 cols
-            Field_populate(pField, 35);     // Populates the field with 35 mines
-        }
-
-    // Game type: Custom
+    // For custom games
     } else {
 
-        // Acquires the file of the chosen custom level
-        //! uncomment this later
-        // File *pCustomLevel = Gameplay_selectCustomLevel();
+        char *sName;
+        // TODO: input custom level name
 
-        // Sets up the field based on the custom level's specs
-        // ! uncomment this too
-        // Gameplay_setUpCustomLevel(pCustomLevel, pField);
+        // TODO: check if the level exists; if not, handle the error
 
+        Gameplay_initCustom(pField, sName);
+    }
+
+    // Initializes each of the tile's inspection status
+    Grid_clear(pField->pInspectGrid, 0);
+
+    // Specifies the number of mines adjacent to each tile
+    Field_setNumbers(pField);
+}
+
+/**
+ * Intializes the field's data according to the difficulty.
+ * This function is used for classic games.
+ * 
+ * @param   { GameDifficulty }    eGameDifficulty   The game's difficulty.
+ * @param   { Field * }     pField   The field to be modified.
+*/
+void Gameplay_initClassic(GameDifficulty eDifficulty, Field *pField) {
+
+    // For easy mode
+    if(eDifficulty == GAMEPLAY_DIFFICULTY_EASY) {
+        
+        // Sets up the field's width and height
+        pField->dWidth = GAME_EASY_ROWS;
+        pField->dHeight = GAME_EASY_COLUMNS;
+        
+        // Populates the field with mines 
+        Field_populateRandom(pField, GAME_EASY_MINES);
+    }
+    
+    // For difficult mode
+    else {
+
+        // Sets up the field's width and height
+        pField->dWidth = GAME_DIFFICULT_ROWS;
+        pField->dHeight = GAME_DIFFICULT_COLUMNS;
+
+        // Populates the field with mines
+        Field_populateRandom(pField, GAME_DIFFICULT_MINES);
     }
 }
 
 /**
- * Selects the custom level based on the player's input.
+ * Intializes the field's data according to the custom game's specs.
  * 
- * @param   { File * }      A pointer to the custom level's file.
- * @return  { File * }      A pointer to the custom level's file.
+ * @param   { Field * }     pField   The field to be modified.
+ * @param   { sName * }     sName    Name of the custom level.
 */
-File *Gameplay_selectCustomLevel() {
-    char *sPath;        // The path of the file
+void Gameplay_initCustom(Field *pField, char *sName) {
+    
+    // Declares the path of the custom level's file.
+    char *sPath = String_alloc(LEVEL_PATH_MAX_SIZE);
 
-    // TODO: acquire the inputted file name
+    // Sets the cutom level's file path
+    snprintf(sPath, LEVEL_PATH_MAX_SIZE, "%s%s.txt", LEVELS_FOLDER_PATH, sName);
 
-    // Completes the file path with the file name.
-    sprintf(sPath, sizeof(*sPath) + sizeof(char)*13, "./levels/%s.txt", sPath);
+    // Populates the field with mines
+    Field_populateCustom(pField, sPath);
 
-    File *pCustomLevel = File_create(sPath);
-
-    return pCustomLevel;
+    // Deallocates the memory for the file path's string
+    String_kill(sPath);
 }
 
 /**
- * Sets up the custom level's field.
+ * Inspects a tile.
  * 
- * @param   { File * }      pCustomLevel    A pointer to the game's field.
- * @param   { Field * }     pField          A pointer to the game's field.
+ * @param   { Field * }     pField   The field to be modified.
+ * @param   { int }         x        The tile's x-coordinate in index notation.
+ * @param   { int }         y        The tile's y-coordinate in index notation.
 */
-void Gameplay_setUpCustomLevel(File *pCustomLevel, Field *pField) {
+void Gameplay_inspect(Field *pField, int x, int y) {
     int i, j;
-    char dCell;
-    int dWidth, dHeight;
 
-    FILE *pFile = fopen(pCustomLevel->sPath, "r");
+    // Considers the specific tile inspected
+    Field_inspect(pField, x, y);
 
-    if(pFile == NULL)
-        return;     // TODO: error handling
+    // Ends the game if a mine has been inspected
+    if(Grid_getBit(pField, x, y))
+        Gameplay_end(GAMEPLAY_ENDS_BY_LOSING);
 
-    // Gets the width and height of the field
-    fscanf(pFile, "%d %d", &dWidth, &dHeight);
+    // Cascades the inspection if the number on the tile is 0
+    if(pField->aNumbers[x][y] == 0) {
 
-    // Places the mines on the field
-    for(i = 0; i < dWidth; i++) {
-        for(j = 0; j < dHeight; j++) {
+        // The following loops check each adjacent tile within bounds of the field
 
-            fscanf(pFile, "%c ", &dCell);
+        // Loops through each row
+        for(i = x - 1; i <= x + 1; i++) {
+            if(i >= 0 && i <= pField->dWidth - 1) {
 
-            // Sets the specific bit of the field to 1 if there is a mine
-            if(dCell == "X")
-                Grid_setBit(pField->pMineGrid, i, j, 1);
+                // Loops through each column
+                for(j = y - 1; j <= y + 1; j++) {
+                    if(j >= 0 && j <= pField->dHeight - 1) {
 
-            // Sets the specific bit of the field to 0 if there is no mine
-            else
-                Grid_setBit(pField->pMineGrid, i, j, 0);
+                        // Recures the function if the number on the tile is 0
+                        if(pField->aNumbers[i][j] == 0)
+                            Gameplay_inspect(pField, i, j);
+                    }
+                }
+            }
         }
     }
-
 }
 
 /**
- * Initiates the game. Handles the player's actions.
+ * Adds a flag on a tile.
  * 
+ * @param   { Field * }     pField   The field to be modified.
+ * @param   { int }         x        The tile's x-coordinate in index notation.
+ * @param   { int }         y        The tile's y-coordinate in index notation.
 */
-void Gameplay_play() {
-
+void Gameplay_addFlag (Field *pField, int x, int y) {
+    Grid_setBit(pField->pFlagGrid, x, y, 1);
 }
 
 /**
- * Inspects a cell for a mine.
+ * Removes a flag from a tile.
  * 
+ * @param   { Field * }     pField   The field to be modified.
+ * @param   { int }         x        The tile's x-coordinate in index notation.
+ * @param   { int }         y        The tile's y-coordinate in index notation.
 */
-void Gameplay_inspect() {
-    // TODO: Code this function considering the GUI.
-}
-
-/**
- * Adds a flag on a cell.
- * 
-*/
-void Gameplay_addFlag() {
-    // TODO: Code this function considering the GUI.
-}
-
-/**
- * Removes a flag from a cell.
- * 
-*/
-void Gameplay_removeFlag() {
-    // TODO: Code this function considering the GUI.
-}
-
-/**
- * Quits the game.
- * 
-*/
-void Gameplay_quit() {
-    // TODO: Code this function considering the GUI.
+void Gameplay_removeFlag (Field *pField, int x, int y) {
+    Grid_setBit(pField->pFlagGrid, x, y, 0);
 }
 
 /**
  * Ends the game.
  * 
+ * @param   { GameEnding }      eEnding     How the game was ended.
 */
-void Gameplay_end() {
+void Gameplay_end(GameEnding eEnding) {
     // TODO: Code this function considering the GUI.
 }
 
